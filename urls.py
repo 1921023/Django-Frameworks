61fb55a13617bf11e0a896732019f34410077483
deepak kumar <54367640+Mr-codehunter@users.noreply.github.com>
2020-06-17 10:32:52 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,28 @@
+"""world URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path, re_path
-from home import views
+from django.urls import path, include
+
+admin.site.site_header = "GNDEC Admin"
+admin.site.site_title = "GNDEC Admin Portal"
+admin.site.index_title = "Welcome to Computer Centre Portal"
+
+
 urlpatterns = [
-    path("", views.index, name='home'),
-    path("staff", views.staff, name='staff'),
-    path("network", views.network, name='network'),
-    path("servers", views.servers, name='servers'),
-    path("software", views.software, name='software'),
-    path("storage", views.storage, name='storage'),
-    path("workstations", views.workstations, name='workstations'),
-    path("aboutus", views.aboutus, name='aboutus'), 
- ]
-
-
-
+    path('admin/', admin.site.urls),
+    path('',include('home.urls')),
+    
+]
